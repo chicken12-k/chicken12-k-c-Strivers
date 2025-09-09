694341991921b32d50f5e2fdf48ad262750328fc
Chiranjeev Kashyap <ckashu10@gmail.com>
2025-09-10 01:54:58 +0530

    Update multiset.cpp

---
+++ STEP 1/Lec 3 STL/multiset.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<set>
+
+using namespace std;
+
+int main(){ 
+
+    multiset<int> ms;
+    ms.insert(1); 
+    ms.insert(1); 
+    ms.insert(1); 
+    ms.insert(2); 
+    ms.insert(3); 
+
+    ms.erase(1); // Erases all occurrences of 1
+    // ms.find(1) gets an iterator to the first '1'
+    // ms.erase(ms.find(1)); // Erases only one occurrence of 1
+
+    // Printing the remaining elements
+    cout << "Elements in the multiset: ";
+    for (int element : ms) {
+        cout << element << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
